f6b02ee2cf0fb7648a24d549d4d10faec22f5bb7
hdpyo <vvhooping@gmail.com>
2022-05-25 18:19:05 +0900

    Add test: <App />
    - it renders title of page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders title of page ', () => {
+    const { container } = render(<App />);
+
+    expect(container).toHaveTextContent('Coffee-and-Taste');
+  });
 });
