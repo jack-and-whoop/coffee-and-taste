43db78e114f2c59c3eb2cda9b2ca083a0bbf3739
hdpyo <vvhooping@gmail.com>
2022-05-27 16:09:27 +0900

    build: update jest.config.js
    - Jest 이미지 import 관련 에러 fix
      - 프로젝트 최상단에 mocks 폴더를 생성하고 그 안에 fileMock.js 라는 파일을 생성
      - jest.config.js 에 moduleNameMapper 옵션 추가

--- jest.config.js
+++ jest.config.js
@@ -81,7 +81,10 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mocks/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/mocks/fileMock.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -130,7 +133,7 @@ module.exports = {
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
     'jest-plugin-context/setup',
-    './jest.setup.js',
+    './jest.setup',
   ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
