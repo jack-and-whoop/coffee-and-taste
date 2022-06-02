4408e71b15fb855c5b5a7d769ac556881af1ed40
hdpyo <vvhooping@gmail.com>
2022-06-02 19:04:15 +0900

    Feat: <App /> 에 Route 를 추가한다.
    - 특정 메뉴 그룹을 클릭했을 때 이동할 주소와 컴포넌트를 추가한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './Home';
+import MenuListPage from './MenuListPage';
 
 export default function App() {
   return (
     <BrowserRouter basename="/coffee-and-taste">
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/menu-groups/:menuGroupId" element={<MenuListPage />} />
       </Routes>
 
     </BrowserRouter>
