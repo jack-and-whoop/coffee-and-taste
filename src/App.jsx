e53372c70d1303c6765304d4d21d7a29ee2abed1
hdpyo <vvhooping@gmail.com>
2022-06-03 14:20:10 +0900

    Refactor: <Home /> 안의 컴포넌트를 <App /> 으로 이동한다.
    - 로고, 타이틀 등을 상단에 고정시킨다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+
 import Home from './Home';
 import MenuListPage from './MenuListPage';
+import CategoryContainer from './CategoryContainer';
+
+import logo from './images/logo.png';
+
+const Container = styled.div({
+  margin: '0 auto',
+  width: '80%',
+});
+
+const Header = styled.header({
+  width: '80%',
+  position: 'fixed',
+  display: 'flex',
+  alignItems: 'center',
+  background: 'beige 100%',
+});
+
+const Logo = styled.div({
+  marginRight: '10px',
+  width: '50px',
+  height: '50px',
+  borderRadius: '50%',
+  background: `url(${logo}) center/100% no-repeat`,
+  // margin: '50px auto 30px auto',
+});
+
+const Title = styled.h1({
+  margin: 0,
+});
 
 export default function App() {
   return (
     <BrowserRouter basename="/coffee-and-taste">
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/menu-groups/:menuGroupId" element={<MenuListPage />} />
-      </Routes>
-
+      <Container>
+        <Header>
+          <Logo />
+          <Title>Coffee-and-Taste</Title>
+        </Header>
+        <CategoryContainer />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/menu-groups/:menuGroupId" element={<MenuListPage />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
