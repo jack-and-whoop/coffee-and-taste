60f4d2a46bb4df6e0cd9d61193467a9b79dcae07
hdpyo <vvhooping@gmail.com>
2022-06-03 20:34:25 +0900

    Refactor: <Home> 에서 렌더링하지 않는 <MenuContainer> 를 제거한다.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,10 @@
 import MenuGroupContainer from './MenuGroupContainer';
-import MenuContainer from './MenuContainer';
 import MenuDetail from './MenuDetail';
 
 export default function Home() {
   return (
     <>
       <MenuGroupContainer />
-      <MenuContainer />
       <MenuDetail />
     </>
   );
