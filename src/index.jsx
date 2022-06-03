181225aee9b9888ebec20b48b732f985629fc926
hdpyo <vvhooping@gmail.com>
2022-06-03 18:23:39 +0900

    Style: Global 로 reset-css 를 적용한다.
    - 브라우저마다 미리 설정된 css 기본값을 초기화한다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+import { css, Global } from '@emotion/react';
+import emotionReset from 'emotion-reset';
+
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global styles={css`${emotionReset}`} />
     <App />
   </React.StrictMode>,
 );
