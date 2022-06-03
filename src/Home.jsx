f7339df3523e670e80535e9f42efd84340c535d5
hdpyo <vvhooping@gmail.com>
2022-06-03 21:56:01 +0900

    Refactor: 사용하지 않는 컴포넌트 제거

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,7 @@
 import MenuGroupContainer from './MenuGroupContainer';
-import MenuDetail from './MenuDetail';
 
 export default function Home() {
   return (
-    <>
-      <MenuGroupContainer />
-      <MenuDetail />
-    </>
+    <MenuGroupContainer />
   );
 }
