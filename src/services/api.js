75745d6bc08af35f8351a1491bad13fd1eab70ea
hdpyo <vvhooping@gmail.com>
2022-06-05 22:13:12 +0900

    Feat: 비동기 처리 동작을 담당할 api.js 생성

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://coffee-and-taste.kro.kr/api';
+
+async function fetchData(url) {
+  return axios.get(url)
+    .then((response) => response.data);
+}
+
+export async function fetchCategories() {
+  return fetchData(`${BASE_URL}/categories`);
+}
+
+export async function fetchMenuGroups({ categoryId }) {
+  return fetchData(`${BASE_URL}/categories/${categoryId}/menu-groups`);
+}
+
+export async function fetchMenuList({ menuGroupId }) {
+  return fetchData(`${BASE_URL}/menu-groups/${menuGroupId}/menus`);
+}
